d5c60aaec16ccf1edae4e5d18cc764d3b9a6cc7a
junwang09 <junwang09@gmail.com>
2018-11-27 21:22:05 -0800

    add jog detail

--- jobs/models.py
+++ jobs/models.py
@@ -4,7 +4,7 @@ class Job(models.Model):
     title = models.CharField(max_length=255)
     image = models.ImageField(upload_to='images/')
     summary = models.TextField()
-    body = models.TextField()
+    body = models.TextField(default='More details are coming soon!')
 
     def __str__(self):
         return self.title
--- myblog/settings.py
+++ myblog/settings.py
@@ -75,10 +75,21 @@ WSGI_APPLICATION = 'myblog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postresql',
+        'NAME': 'myblog',
+        'USER': 'hongjia',
+        'PASSWORD': 'WORK4yourself',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
